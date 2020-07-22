7f9b35146f6d50dd406eec3a66f7483906c837b6
Harry Pierson <harrypierson@ngd.neo.org>
2020-07-21 21:11:53 -0700

    ensure correct branch for update.ps1

--- update.ps1
+++ update.ps1
@@ -1,6 +1,12 @@
 param([string]$branch = "master")
 
-git subtree pull --prefix vm official-vm $branch --squash
-git subtree pull --prefix core official-core $branch --squash
-git subtree pull --prefix modules official-modules $branch --squash
-git subtree pull --prefix devpack official-devpack $branch --squash
+$currentBranch = git symbolic-ref -q --short HEAD 2> $null
+
+if ($currentBranch -ne $branch) {
+    Write-error "wrong branch $branch"
+} else {
+    git subtree pull --prefix vm official-vm $branch --squash
+    git subtree pull --prefix core official-core $branch --squash
+    git subtree pull --prefix modules official-modules $branch --squash
+    git subtree pull --prefix devpack official-devpack $branch --squash
+}
