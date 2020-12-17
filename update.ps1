ada7c47a7ede3da1b1b93ef80dd13a5cac20c8e3
Harry <harrypierson@outlook.com>
2020-12-17 10:01:31 -0800

    update.ps1

--- update.ps1
+++ update.ps1
@@ -1,22 +1,29 @@
-param([string]$branch = "master", [switch]$merge)
+param([switch]$merge)
 
 $currentBranch = git symbolic-ref -q --short HEAD 2> $null
 
-if ($currentBranch -ne $branch) {
-    throw "wrong branch $branch"
-}
+# if ($currentBranch -ne $branch) {
+#     throw "wrong branch $branch"
+# }
 
 # vm project tagged preview 4 at 27dd6758291cf6cf64390598707f98faac3845c0
 # core project tagged preview 4 at 414dab140025f46898a3afee3a59a4e18b72d2b2
 # exclude from subtree pull for now
-$projects = "core","devpack","modules"#,"node" ,"vm"
+# $projects = "core","devpack","modules"#,"node" ,"vm"
 
-git fetch --all --tags
-foreach ($prj in $projects) {
-    git subtree pull --prefix $prj "official-$prj" $branch --squash
+$projects = @{ 
+    core = "414dab140025f46898a3afee3a59a4e18b72d2b2"; 
+    vm = "27dd6758291cf6cf64390598707f98faac3845c0"; 
+    devpack = "master";
+    modules = "master";
+    node = "master" }
+
+git fetch --all
+foreach ($prj in $projects.keys) {
+    git subtree pull --prefix $prj "official-$prj" "$($projects[$prj])" --squash
 }
 
 if ($merge) {
-    git checkout "monorepo-$branch"
-    git merge $branch
+    git checkout "monorepo-$currentBranch"
+    git merge $currentBranch
 }
