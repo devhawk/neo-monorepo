a5bfb0a738ffcf9dfd5bfa6f13ba1afdc81172a3
Harry <harrypierson@outlook.com>
2020-12-21 12:12:42 -0800

    update script

--- update.ps1
+++ update.ps1
@@ -6,9 +6,7 @@ if ($currentBranch -ne $branch) {
     throw "wrong branch $branch"
 }
 
-# vm project switched to .net 5 after a14b934f5b5c8779164e3b96b276984b3a57ccd4
-# exclude from subtree pull for now
-$projects = "core","devpack","modules","node" #,"vm"
+$projects = "core","devpack","modules","node" ,"vm"
 
 git fetch --all
 foreach ($prj in $projects) {
