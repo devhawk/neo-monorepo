298c712a0e2611a72327a0c268b5d43bd93006d7
Harry <harrypierson@outlook.com>
2021-04-13 12:31:24 -0700

    Squashed 'devpack/' changes from 522a12fc..c61725e0
    c61725e0 Accept multiple .cs files (#552)
    fc3a0609 Fix abstract classes (#555)
    3f8ad818 Fix methods of generic types (#554)
    a086f192 add null check ut (#553)
    893fcb02 UT (#528)
    23f30fc6 Include source files of Neo.SmartContract.Framework in nuget package (#546)
    153ae159 index UT (#548)
    fceecd6a Read source files from csproj (#544)
    d0db4c87 Fix the bug of cast (#536)
    7bd4936d Support goto statement (#545)
    19859757 Allow attributes in contracts (#543)
    be3c91e5 Remove unused file (#542)
    eb735986 Support string indexer (#535)
    61ae55a1 Fix path (#537)
    3ed8d8b6 Support base expression (#532)
    6965fdee Support polymorphism (#523)
    179baf48 Sync to Neo 3.0.0-CI01264 (#526)
    a893cb44 Fix extension methods (#525)
    cce1de63 Support byte array initializer (#519)
    542cde80 Move methods from SmartContract to ExecutionEngine (#522)
    df4ac354 Convert all methods in the source code (#515)
    72eb28e2 Fix equals operator (#516)
    b565749d Allow public instance methods (#511)
    9ffd25e9 Fix break and continue (#512)
    e4b9bdd2 Add Iterator UTs (#513)
    c18f63c4 Sync to Neo 3.0.0-CI01263 (#506)
    a46e32e2 Optimize StorageMap: use byte as the prefix (#510)
    84bb41d8 Fix integer parse (#508)
    efd375df add bigintege cast and null cast ut (#507)
    27cac5f5 Array support foreach (#504)
    9e17b210 Fix BigInteger explicit cast (#505)
    c3f7f744 Add explicit cast from BigInteger to ByteString (#500)
    36207a61 Iterator support foreach statement (#495)
    ebc51258 Optimize ByteString explicit cast (#496)
    a633f430 Fix string add (#497)
    d507a070 Fix the way to search for smart contracts (#494)
    5a4ec6a2 Optimize StorageMap (#491)
    84cdc6ab Support this property (#492)
    aeeaa93a Fix explicit cast for ECPoint, UInt160 and UInt256 (#493)
    b3e637a9 Neo.Compiler.CSharp (#482)
    7470319b Add ECPoint.IsValid (#487)
    91605cfa throw exception if size incorrect for ECPoint, UInt160 and UInt256 (#484)
    
    git-subtree-dir: devpack
    git-subtree-split: c61725e039dc47de198c4ac360eb2146953bbfff

--- src/Neo.SmartContract.Framework/SmartContract.cs
+++ src/Neo.SmartContract.Framework/SmartContract.cs
@@ -1,33 +1,12 @@
-using Neo.SmartContract.Framework.Native;
 using System.Runtime.CompilerServices;
 
 namespace Neo.SmartContract.Framework
 {
     public abstract class SmartContract
     {
-        /// <summary>
-        /// Faults if `condition` is false
-        /// </summary>
-        /// <param name="condition">Condition that MUST meet</param>
         [MethodImpl(MethodImplOptions.InternalCall)]
-        [OpCode(OpCode.ASSERT)]
-        public static extern void Assert(bool condition);
-
-        /// <summary>
-        /// Abort the execution
-        /// </summary>
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        [OpCode(OpCode.ABORT)]
-        public static extern void Abort();
-
-        public static ByteString Hash160(ByteString value)
-        {
-            return CryptoLib.ripemd160(CryptoLib.Sha256(value));
-        }
-
-        public static ByteString Hash256(ByteString value)
-        {
-            return CryptoLib.Sha256(CryptoLib.Sha256(value));
-        }
+#pragma warning disable IDE1006
+        public extern static void _initialize();
+#pragma warning restore IDE1006
     }
 }
