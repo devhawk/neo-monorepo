cf4ff3d29badfc5459a4f4637f8549202ae0b187
Harry <harrypierson@outlook.com>
2021-04-16 14:45:18 -0700

    add devpack-msil to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,8 @@
-$solutions = "vm\neo-vm.sln","core\neo.sln","modules\neo-modules.sln","devpack\neo-devpack-dotnet.sln" ,"node\neo-node.sln"
+$solutions = "vm\neo-vm.sln",
+    "core\neo.sln",
+    "modules\neo-modules.sln",
+    "devpack\neo-devpack-dotnet.sln".
+    "devpack-msil\neo-devpack-dotnet.sln",
+    "node\neo-node.sln"
+
 $solutions | %{write-host $_ -ForegroundColor Cyan; dotnet build $_}
