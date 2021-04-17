e7601fad791720eb665342c002b62a9882b5942b
Harry <harrypierson@outlook.com>
2021-04-16 18:46:30 -0700

    add write-host to update.ps1

--- update.ps1
+++ update.ps1
@@ -10,13 +10,16 @@ $projects = "core","devpack","modules","node" ,"vm"
 
 git fetch --all
 foreach ($prj in $projects) {
+    write-host $prj -ForegroundColor Cyan;
     git subtree pull --prefix $prj "official-$prj" $branch --squash
 }
 
 # since NEON is moved to a different branch, need to handle it special
+write-host devpack-msil -ForegroundColor Cyan;
 git subtree pull --prefix devpack-msil official-devpack msil --squash 
 
 if ($merge) {
+    write-host $"Merging $branch into monorepo-$branch" -ForegroundColor Cyan; 
     git push
     git checkout "monorepo-$branch"
     git merge $branch
