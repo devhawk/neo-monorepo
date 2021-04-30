d80b864850ee0fe57bb29021c1ddcd3d43b8ffdd
Harry <harrypierson@outlook.com>
2021-04-30 09:14:42 -0700

    patch https://github.com/neo-project/neo-modules/issues/587

--- modules/src/OracleService/OracleService.cs
+++ modules/src/OracleService/OracleService.cs
@@ -327,7 +327,8 @@ namespace Neo.Plugins
             {
                 Version = 0,
                 Nonce = unchecked((uint)response.Id),
-                ValidUntilBlock = requestTx.BlockIndex + Transaction.MaxValidUntilBlockIncrement,
+                // MONOREPO PATCH
+                ValidUntilBlock = requestTx.BlockIndex + settings.MaxValidUntilBlockIncrement,
                 Signers = new[]
                 {
                     new Signer
