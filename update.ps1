9c6fdcd16a89f16c4dd2cf1d21355b696be3d5dc
Harry <harrypierson@outlook.com>
2021-12-02 15:00:36 -0800

    modify update

--- update.ps1
+++ update.ps1
@@ -7,11 +7,11 @@ if ($currentBranch -ne $branch) {
 }
 
 $projects = @{
-    vm = "v3.0.1"
-    core = "v3.0.3";
-    devpack = "v3.0.3";
-    modules = "v3.0.3";
-    node = "v3.0.3";
+    vm = "v3.1.0"
+    core = "v3.1.0";
+    devpack = "v3.1.0";
+    modules = "v3.1.0";
+    node = "v3.1.0";
 }
 
 git fetch --all
