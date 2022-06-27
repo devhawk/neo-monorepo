f82b5e5b497b806bb09feaedc8176eb1ce7d0602
Harry <harrypierson@outlook.com>
2022-06-27 10:42:04 -0700

    modify update

--- update.ps1
+++ update.ps1
@@ -1,4 +1,4 @@
-param([string]$branch = "rel-330", [switch]$merge)
+param([string]$branch = "rel-331", [switch]$merge)
 
 $currentBranch = git symbolic-ref -q --short HEAD 2> $null
 
@@ -7,11 +7,11 @@ if ($currentBranch -ne $branch) {
 }
 
 $projects = @{
-    vm = "v3.3.0"
-    core = "v3.3.0";
+    vm = "v3.3.1"
+    core = "v3.3.1";
     devpack = "v3.3.0";
-    modules = "v3.3.0";
-    node = "v3.3.0";
+    modules = "v3.3.1";
+    node = "v3.3.1";
 }
 
 git fetch --all
