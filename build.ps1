08a77bb06f6aedd64e5db0eb0c794ccd8255a77b
Harry <harrypierson@outlook.com>
2023-03-01 11:29:18 -0800

    Merge branch 'monorepo-traversal'

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$solutions = "vm\neo-vm.sln",
-    "core\neo.sln",
-    "modules\neo-modules.sln",
-    "devpack\neo-devpack-dotnet.sln".
-    "node\neo-node.sln"
-
-$solutions | %{write-host $_ -ForegroundColor Cyan; dotnet build $_}
